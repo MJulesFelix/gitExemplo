e531c57c83f1a667e021b1aa5f167eeba1cf07e5
“Maria <mjuliakm@gmail.com>
2025-03-22 23:22:08 -0300

    Criação do Projeto

---
+++ src/gitexemplo/GitExemplo.java
@@ -0,0 +1,16 @@
+
+package gitexemplo;
+
+
+public class GitExemplo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        System.out.println("segunda versão");
+        System.out.println("testando o envio de versões");
+    }
+    
+}
